223aef05667604f19c6b9bf43b609bfc8a057101
judavago1 <juan.vargasg@uniagustiniana.edu.co>
2025-11-09 03:13:44 +0000

    li

---
+++ app/bible/chapters/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function ChaptersPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Capítulos</h1>
+      <p>Aquí se mostrarán los capítulos del libro seleccionado.</p>
+    </div>
+  );
+}
